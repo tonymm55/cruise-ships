5349cfd95ce22a12ef3ed6f0f5c166cabe912853
Anthony Moran <tonymmoran@gmail.com>
2022-12-26 11:34:20 +0000

    created code so to pass the test startingPort toBeFalsy

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -5,10 +5,16 @@ describe('constructor',() => {
   it('can be instantiated', () => {
     expect(new Ship ('Dover')).toBeInstanceOf(Object);
   });
-});
-//   it('has a starting port', () => {
-//     const ship = new Ship('Dover');
+  it('has a starting port', () => {
+    const ship = new Ship('Dover');
 
-//     expect(ship.startingPort).toBe('Dover');
-//   });
-// });
+    expect(ship.startingPort).toBe('Dover');
+  });
+  it('can set sail', () => {
+    const ship = new Ship ('Dover');
+    
+    ship.setSail();
+
+    expect(ship.startingPort).toBeFalsy();
+  })
+});
--- src/ship.js
+++ src/ship.js
@@ -3,9 +3,15 @@ class Ship {
       this.startingPort = startingPort;
     //   this.numPassengers = numPassengers;
     } 
+    setSail() {
+      this.startingPort = null;
+    }
   }
 
 const ship = new Ship('Dover');
+
+ship.setSail();
+
 
 module.exports = Ship;
 // const ship = new Ship('Calais', 200);
